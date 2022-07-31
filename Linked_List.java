11e6daf9966c15126779c0fb311390fc7ea8d666
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-31 21:21:44 +0530

    Created linked-list by appending 30 and 70 to 56.

--- Linked_List.java
+++ Linked_List.java
@@ -3,15 +3,28 @@ package com.bridgelabz.linkedlist;
 public class Linked_List {
 	Node head = null;  // initially we are assigning head as null.
 	
-	// Ability to create Linked List by adding 30 and 56 to 70 
+	//Ability to create Linked List by adding 30 and 56 to 70 
 	public <T> void addFirst(T key) {             
 		Node node = new Node(key);           // Adding data at first method.
-		node.setKey(key);                       
+		
 		node.setNext(head);           //    56 | next ------> 30 | next -------> 70 | next -->null
 		head = node;                  //  (added third)     (added second)     (added first)
 	}
 	
-	// printing linked list
+	
+	// Ability to create Linked List by appending 30 and 70 to 56. 
+	public <T> void append(T key) {
+		Node currentNode = head;       // temporary variable taken as head.
+		while(currentNode.next != null) {       
+			currentNode = currentNode.next;
+		}
+		Node node = new Node(key);               // 56 | next -------> 30 | next -------> 70 | next ----> null
+												//                  (appended to 56)    (appended to 30)						
+		currentNode.next = node;
+	}
+	
+	
+	// printing linked list 
 	public <T> void print() {
 		Node currentNode = head;
 		System.out.println("Linked-List :- ");           // head
@@ -23,17 +36,16 @@ public class Linked_List {
 	}
 	
 	public static void main(String[] args) {
-		
 		// Displayed welcome message 
 		System.out.println("\t WELCOME TO LINKED LIST \n");
 		
 		// creating object of Linked_List class 
 		Linked_List linkedList = new Linked_List();   
-		
-		linkedList.addFirst(70);  
-		linkedList.addFirst(30);        
-		linkedList.addFirst(56);								
-		
+		 
+		linkedList.addFirst(56);	
+		linkedList.append(30);      // appending 30 and 70 to 56.
+		linkedList.append(70);        
+									
 		linkedList.print();  //printing linkedlist keys/elements/data.
 	}	
 }
