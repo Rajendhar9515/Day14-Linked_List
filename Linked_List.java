fbfd813562a621431850024d716aea8d79d828d4
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-31 22:02:10 +0530

    Deleted first key from the linked-list.

--- Linked_List.java
+++ Linked_List.java
@@ -22,6 +22,25 @@ public class Linked_List {
 												//                  (appended to 56)    (appended to 30)						
 		currentNode.next = node;
 	}
+
+	// Ability to insert 30 between 56 and 70. 
+	public <T> void insertBetween(T previous_key , T key) {
+		Node currentNode = head;
+		while(currentNode.key != previous_key) {
+			currentNode = currentNode.next;          //  56 | next  ------>  30 | next------> 70 | next ----> null
+		}                                            //(previous_key)    (inserted between)
+		Node node = new Node(key);
+		node.next = currentNode.next;
+		currentNode.next = node;
+	}
+
+	// Ability to delete the first element in the LinkedList of sequence 56->30->70.
+	public <T> Node popFirst() {
+		Node currentNode = head;
+		head = head.next;          // changing head from 56 to 30 , so 56 will be null and it will be deleted.
+		return currentNode;
+	}
+	
 	
 	
 	// printing linked list 
@@ -36,16 +55,18 @@ public class Linked_List {
 	}
 	
 	public static void main(String[] args) {
+		
 		// Displayed welcome message 
 		System.out.println("\t WELCOME TO LINKED LIST \n");
 		
 		// creating object of Linked_List class 
 		Linked_List linkedList = new Linked_List();   
 		 
-		linkedList.addFirst(56);	
-		linkedList.append(30);      // appending 30 and 70 to 56.
-		linkedList.append(70);        
+		linkedList.addFirst(56);
+		linkedList.append(70);
+		linkedList.insertBetween(56, 30);     
+		linkedList.popFirst();        
 									
 		linkedList.print();  //printing linkedlist keys/elements/data.
-	}	
+	}
 }
